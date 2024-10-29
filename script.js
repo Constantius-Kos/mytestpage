b8b785b5e215973008661aec676b3d0f64ef2a54
constantius <constantiuskos@gmail.con>
2024-10-29 19:14:18 +0400

    Добавлены цитаты, изменен массив, разделены цитаты и автор

--- script.js
+++ script.js
@@ -1,16 +1,82 @@
 const quotes = [
-  'The only way to do great work is to love what you do. - Steve Jobs',
-  'Innovation distinguishes between a leader and a follower. - Steve Jobs',
-  "Your time is limited, so don't waste it living someone else's life. - Steve Jobs",
+  {
+    quote: 'The only way to do great work is to love what you do',
+    author: 'Steve Jobs',
+  },
+  {
+    quote: 'Innovation distinguishes between a leader and a follower',
+    author: 'Steve Jobs',
+  },
+  {
+    quote: "Your time is limited, so don't waste it living someone else's life",
+    author: 'Steve Jobs',
+  },
+  {
+    quote:
+      'Success is not final, failure is not fatal: it is the courage to continue that counts',
+    author: 'Winston Churchill',
+  },
+  {
+    quote: 'The best way to predict the future is to invent it',
+    author: 'Alan Kay',
+  },
+  {
+    quote: "If you're going through hell, keep going",
+    author: 'Winston Churchill',
+  },
+  {
+    quote: 'The journey of a thousand miles begins with one step',
+    author: 'Lao Tzu',
+  },
+  {
+    quote: 'Life is what happens when you’re busy making other plans',
+    author: 'John Lennon',
+  },
+  {
+    quote:
+      'The only limit to our realization of tomorrow is our doubts of today',
+    author: 'Franklin D. Roosevelt',
+  },
+  {
+    quote:
+      'Do not go where the path may lead, go instead where there is no path and leave a trail',
+    author: 'Ralph Waldo Emerson',
+  },
+  {
+    quote: 'It is never too late to be what you might have been',
+    author: 'George Eliot',
+  },
+  {
+    quote: 'Believe you can and you’re halfway there',
+    author: 'Theodore Roosevelt',
+  },
+  {
+    quote:
+      'Success usually comes to those who are too busy to be looking for it',
+    author: 'Henry David Thoreau',
+  },
+  {
+    quote: 'Do what you can, with what you have, where you are',
+    author: 'Theodore Roosevelt',
+  },
+  {
+    quote:
+      'The future belongs to those who believe in the beauty of their dreams',
+    author: 'Eleanor Roosevelt',
+  },
 ];
-
+const quoteAuthorElement = document.getElementById('author');
 const quoteElement = document.getElementById('quote');
 const generateBtn = document.getElementById('generate-btn');
 
 function generateRandomQuote() {
   const randomIndex = Math.floor(Math.random() * quotes.length);
   const randomQuote = quotes[randomIndex];
-  quoteElement.textContent = randomQuote;
+  // const quote = randomQuote.quote;
+  // const quoteAuthor = randomQuote.author;
+  // const { quote, author} = randomQuote;
+  quoteElement.textContent = randomQuote.quote;
+  quoteAuthorElement.textContent = randomQuote.author;
 }
 
 generateBtn.addEventListener('click', generateRandomQuote);
