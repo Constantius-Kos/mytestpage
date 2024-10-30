ff7bdffa79f9c0a0fa79e1dbb3db47f2f96d801e
constantius <constantiuskos@gmail.con>
2024-10-30 20:29:30 +0400

    Добавлены карточки для избранных цитат Добавлена кнока добавления в избранные на карточки добавлена кнопка удаления из избранных Изменен дизайн карточек

--- quotes.js
+++ quotes.js
@@ -2,67 +2,82 @@ const quotes = [
   {
     quote: 'The only way to do great work is to love what you do',
     author: 'Steve Jobs',
+    isFavorite: false,
   },
   {
     quote: 'Innovation distinguishes between a leader and a follower',
     author: 'Steve Jobs',
+    isFavorite: false,
   },
   {
     quote: "Your time is limited, so don't waste it living someone else's life",
     author: 'Steve Jobs',
+    isFavorite: false,
   },
   {
     quote:
       'Success is not final, failure is not fatal: it is the courage to continue that counts',
     author: 'Winston Churchill',
+    isFavorite: false,
   },
   {
     quote: 'The best way to predict the future is to invent it',
     author: 'Alan Kay',
+    isFavorite: false,
   },
   {
     quote: "If you're going through hell, keep going",
     author: 'Winston Churchill',
+    isFavorite: false,
   },
   {
     quote: 'The journey of a thousand miles begins with one step',
     author: 'Lao Tzu',
+    isFavorite: false,
   },
   {
     quote: 'Life is what happens when you’re busy making other plans',
     author: 'John Lennon',
+    isFavorite: false,
   },
   {
     quote:
       'The only limit to our realization of tomorrow is our doubts of today',
     author: 'Franklin D. Roosevelt',
+    isFavorite: false,
   },
   {
     quote:
       'Do not go where the path may lead, go instead where there is no path and leave a trail',
     author: 'Ralph Waldo Emerson',
+    isFavorite: false,
   },
   {
     quote: 'It is never too late to be what you might have been',
     author: 'George Eliot',
+    isFavorite: false,
   },
   {
     quote: 'Believe you can and you’re halfway there',
     author: 'Theodore Roosevelt',
+    isFavorite: false,
   },
   {
     quote:
       'Success usually comes to those who are too busy to be looking for it',
     author: 'Henry David Thoreau',
+    isFavorite: false,
   },
   {
     quote: 'Do what you can, with what you have, where you are',
     author: 'Theodore Roosevelt',
+    isFavorite: false,
   },
   {
     quote:
       'The future belongs to those who believe in the beauty of their dreams',
     author: 'Eleanor Roosevelt',
+    isFavorite: false,
   },
 ];
 
